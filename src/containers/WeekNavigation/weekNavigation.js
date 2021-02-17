1d101823c63bee5fa4b432eb382f23d0a4f36642
MaDud <martaduda@interia.eu>
2021-02-17 21:53:43 +0100

    Implement style into weekNavigation container

--- src/containers/WeekNavigation/weekNavigation.js
+++ src/containers/WeekNavigation/weekNavigation.js
@@ -5,15 +5,15 @@ import * as classes from './weekNavigation.module.scss';
 const weekNavigation = (props) => {
     return (
         <nav className={classes.weekNav}>
-            <div onClick={props.changeWeek} className={classes.weekData}>prevWeek</div>
+            <div onClick={props.changeWeek}>prevWeek</div>
             <div onClick={props.changeWeek} className={classes.dayData}>prevDay</div>
             <hgroup>
-                <h2>{props.weekNo}</h2>
-                <h3 className={classes.dayData}>{props.weekDay}</h3>
-                <h3 className={classes.weekData}>{props.weekPeriod}</h3>
+                <h2>{props.weekNo}Week 10</h2>
+                <h3 className={classes.dayData}>{props.weekDay}Friday</h3>
+                <h3 className={classes.weekData}>{props.weekPeriod}16-17.02.2021</h3>
             </hgroup>
             <div onClick={props.changeWeek} className={classes.dayData}>nextDay</div>
-            <div onClick={props.changeWeek} className={classes.weekData}>nextWeek</div>
+            <div onClick={props.changeWeek}>nextWeek</div>
         </nav>
     )
 }
