20b529c5249191b939cd54f6e65cfab66cbdf164
MaDud <martaduda@interia.eu>
2021-02-18 20:44:13 +0100

    Change structure of weekNavigation

--- src/containers/WeekNavigation/weekNavigation.js
+++ src/containers/WeekNavigation/weekNavigation.js
@@ -5,23 +5,15 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 const weekNavigation = (props) => {
     return (
         <nav className={classes.weekNav}>
-            <div onClick={props.changeWeek}>
-                <FontAwesomeIcon icon='angle-double-left' />
-            </div>
-            <div onClick={props.changeWeek} className={classes.dayData}>
-                <FontAwesomeIcon icon='angle-left' />
-            </div>
+            <FontAwesomeIcon icon='angle-double-left' onClick={props.changeWeek}/>
+            <FontAwesomeIcon icon='angle-left' className={classes.dayData} onClick={props.changeWeek}/>
             <hgroup>
                 <h2>{props.weekNo}Week 10</h2>
                 <h3 className={classes.dayData}>{props.weekDay}Friday</h3>
                 <h3 className={classes.weekData}>{props.weekPeriod}16-17.02.2021</h3>
             </hgroup>
-            <div onClick={props.changeWeek} className={classes.dayData}>
-                <FontAwesomeIcon icon='angle-right' />
-            </div>
-            <div onClick={props.changeWeek}>
-                <FontAwesomeIcon icon='angle-double-right' />
-            </div>
+            <FontAwesomeIcon icon='angle-right' className={classes.dayData} onClick={props.changeWeek}/>
+            <FontAwesomeIcon icon='angle-double-right' onClick={props.changeWeek}/>
         </nav>
     )
 }
