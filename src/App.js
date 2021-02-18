dc44c94afa50b0189574daa780c637b696e00332
MaDud <martaduda@interia.eu>
2021-02-18 11:11:52 +0100

    Create FontAwesome library in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.scss';
 import WeekNavigation from './containers/WeekNavigation/WeekNavigation';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faAngleDoubleRight, faAngleDoubleLeft, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  library.add(faAngleDoubleRight, faAngleDoubleLeft, faAngleLeft, faAngleRight)
   return (
     <div className="App">
       <WeekNavigation />
