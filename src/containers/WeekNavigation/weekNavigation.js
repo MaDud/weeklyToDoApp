a162f6d7d27397e2c8463be8104645bf0336c795
MaDud <martaduda@interia.eu>
2021-02-19 21:33:33 +0100

    Add action to click on arrows

--- src/containers/WeekNavigation/weekNavigation.js
+++ src/containers/WeekNavigation/weekNavigation.js
@@ -10,7 +10,7 @@ const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satu
 
 const WeekNavigation = (props) => {
 
-    const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'))
+    const [date, setDate] = useState(dayjs())
 
     const dayOfWeek = () => {
         const dayNumber = dayjs(date).day()
@@ -31,6 +31,40 @@ const WeekNavigation = (props) => {
         }
     }
 
+    const changeView = e => {
+        const id = e.target.id;
+
+        if ( id === 'prevWeek' || id === 'nextWeek') {
+            const currentWeek = dayjs(date).week();
+            const currentYear = dayjs(date).year();
+
+            if (currentWeek === 1 && id === 'prevWeek') {
+                setDate(dayjs(date).year(currentYear-1).week(52));
+                return
+            } 
+            if (currentWeek === 52 && id === 'nextWeek') {
+                setDate(dayjs(date).year(currentYear+1).week(1));
+                return
+            }
+
+            id === 'prevWeek' ? setDate(dayjs(date).week(currentWeek -1 )) : setDate(dayjs(date).week(currentWeek + 1))
+        } else {
+            const currentDay = dayjs(date).date();
+            const monthLength = dayjs(date).daysInMonth();
+
+            if (currentDay === 1 && id === 'prevDay') {
+                const prevMonthLength = dayjs(date).month(-1).daysInMonth()
+                setDate(dayjs(date).month(-1).date(prevMonthLength));
+                return
+            }
+            if (currentDay === monthLength && id === 'nextDay') {
+                setDate(dayjs(date).month(+1).date(1));
+                return
+            } 
+            id === 'prevDay' ? setDate(dayjs(date).date(currentDay-1)) : setDate(dayjs(date).date(currentDay+1))
+        }
+    }
+
     const weekNumber = () => {
         const dayNumber = dayjs(date).day();
         const week = dayjs(date).week();
@@ -43,15 +77,15 @@ const WeekNavigation = (props) => {
 
     return (
         <nav className={classes.weekNav}>
-            <FontAwesomeIcon icon='angle-double-left' onClick={props.changeWeek}/>
-            <FontAwesomeIcon icon='angle-left' className={classes.dayData} onClick={props.changeWeek}/>
+            <FontAwesomeIcon icon='angle-double-left' onClick={(e) => changeView(e)} id='prevWeek'/>
+            <FontAwesomeIcon icon='angle-left' className={classes.dayData} onClick={(e) => changeView(e)} id='prevDay'/>
             <hgroup>
                 <h2>Week {weekNumber()}</h2>
                 <h3 className={classes.dayData}>{dayOfWeek()}</h3>
                 <h3 className={classes.weekData}>{weekPeriod()}</h3>
             </hgroup>
-            <FontAwesomeIcon icon='angle-right' className={classes.dayData} onClick={props.changeWeek}/>
-            <FontAwesomeIcon icon='angle-double-right' onClick={props.changeWeek}/>
+            <FontAwesomeIcon icon='angle-right' className={classes.dayData} onClick={(e) => changeView(e)} id='nextDay'/>
+            <FontAwesomeIcon icon='angle-double-right' onClick={(e) => changeView(e)} id='nextWeek'/>
         </nav>
     )
 }
