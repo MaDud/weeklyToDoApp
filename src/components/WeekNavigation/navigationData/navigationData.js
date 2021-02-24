2a88ebc353cc7b8bc4fc35360ab518839747269b
MaDud <martaduda@interia.eu>
2021-02-24 11:30:29 +0100

    Implement BEM

---
+++ src/components/WeekNavigation/navigationData/navigationData.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navigationData.scss';
+
+const navigationData = (props) => {
+
+    return (
+        <hgroup className='weekInfo'>
+            <h2 className='weekInfo__mainTitle'>
+                Week {props.weekNumber}
+            </h2>
+            <h3 className={['weekInfo__subtitle', 'weekInfo__subtitle--day'].join(' ')}>
+                {props.dayOfWeek}
+            </h3>
+            <h3 className={['weekInfo__subtitle', 'weekInfo__subtitle--week'].join(' ')}>
+                {props.weekPeriod}
+            </h3>
+        </hgroup>
+    )
+};
+
+export default navigationData
--- src/components/WeekNavigation/weekNavigation.js
+++ src/components/WeekNavigation/weekNavigation.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import * as classes from './weekNavigation.module.scss';
+import './weekNavigation.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import NavigationData from './navigationData/navigationData';
 
 const weekNavigation = (props) => {
     return (
-        <nav className={classes.weekNav}>
-            <FontAwesomeIcon icon='angle-double-left' onClick={() => props.changeView('prevWeek')}/>
-            <FontAwesomeIcon icon='angle-left' className={classes.dayData} onClick={() => props.changeView('prevDay')}/>
-            <hgroup>
-                <h2>Week {props.weekNumber}</h2>
-                <h3 className={classes.dayData}>{props.dayOfWeek}</h3>
-                <h3 className={classes.weekData}>{props.weekPeriod}</h3>
-            </hgroup>
-            <FontAwesomeIcon icon='angle-right' className={classes.dayData} onClick={() => props.changeView('nextDay')}/>
-            <FontAwesomeIcon icon='angle-double-right' onClick={() => props.changeView('nextWeek')}/>
+        <nav className={'weekNav'}>
+            <FontAwesomeIcon 
+                icon='angle-double-left' 
+                onClick={() => props.changeView('prevWeek')}
+                className= 'weekNav__arrow'/>
+            <FontAwesomeIcon 
+                icon='angle-left' 
+                className={['weekNav__arrow', 'weekNav__arrow--day'].join(' ')} 
+                onClick={() => props.changeView('prevDay')}/>
+            <NavigationData 
+                weekNumber = {props.weekNumber}
+                dayOfWeek = {props.dayOfWeek}
+                weekPeriod = {props.weekPeriod}/>
+            <FontAwesomeIcon 
+                icon='angle-right' 
+                className={['weekNav__arrow', 'weekNav__arrow--day'].join(' ')} 
+                onClick={() => props.changeView('nextDay')}/>
+            <FontAwesomeIcon 
+                icon='angle-double-right' 
+                onClick={() => props.changeView('nextWeek')}
+                className= 'weekNav__arrow'/>
         </nav>
     )
 }
