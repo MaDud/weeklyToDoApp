9d20e4894bb169c01e13f9454ac8fb9f0ba6bf80
MaDud <martaduda@interia.eu>
2021-03-02 10:59:18 +0100

    Create file for TasksList logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {DateProvider} from './context/dateContex';
 import TasksNavigation from './components/TasksNavigation/TasksNavigation';
+import TasksList from './components/TasksList/TasksList';
 
 function App() {
   return (
     <DateProvider>
       <TasksNavigation />
+      <TasksList />
     </DateProvider>
   );
 }
---
+++ src/components/TasksList/TasksList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TasksList = () => {
+
+    return (
+        <table>
+
+        </table>
+    )
+}
+
+export default TasksList
