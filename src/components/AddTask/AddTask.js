b227c3c847bf97cd651e0f3e4f81c0b93f2ea89c
MaDud <martaduda@interia.eu>
2021-03-12 19:10:43 +0100

    Preper AddTask component for add task to firebse'

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import Button from '../UI/Button';
 import Modal from '../UI/modal';
-import AddTaskForm from './addTaskForm';
+import AddTaskForm from './AddTaskFrom';
 import '../../styles/AddTask/addTask.scss';
 
 class AddTask extends React.Component {
 
-    state= {
-        show: false
+    constructor(props) {
+        super(props);
+        this.state= {
+            show: false,
+            newTask: {
+                title: '',
+                description: ''
+            }
+        };
+        this.changeAddFormVisibility = this.changeAddFormVisibility.bind(this);
+        this.inputChange = this.inputChange.bind(this)
     }
 
-    changeAddFormVisibility = () => {
+    changeAddFormVisibility () {
         this.setState({show: !this.state.show})
     }
 
+    inputChange (e) {
+        const newTask = this.state.newTask;
+        const id = e.target.id;
+        const value = e.target.value.trim();
+
+        newTask[id] = value;
+
+        this.setState({newTask: newTask})       
+    }
+
     render() {
-        console.log(this.state.show)
+        console.log(this.state.newTask)
         return(
             <React.Fragment>
                 <Modal show={this.state.show}
                         clicked={this.changeAddFormVisibility}>
                     <Button clicked={this.changeAddFormVisibility} btnStyle='button--transparent'>x</Button>
-                    <AddTaskForm/>
+                    <AddTaskForm title={this.state.newTask.title} 
+                            description={this.state.newTask.description}
+                            inputChange={this.inputChange}/>
                 </Modal>
                 <Button clicked={this.changeAddFormVisibility} btnStyle='button--dark'>
                     Add task
--- src/components/AddTask/AddTaskFrom.js
+++ src/components/AddTask/AddTaskFrom.js
@@ -12,8 +12,8 @@ class AddTaskForm extends React.Component {
         const {inputChange, clicked, title, description} = this.props;
         return (
             <form className='addTaskForm'>
-                <label htmlFor='task' className='addTaskForm__label'>Task</label>
-                <input type='text' id='task' onChange={inputChange} className='addTaskForm__text' value={title}/>
+                <label htmlFor='title' className='addTaskForm__label'>Task</label>
+                <input type='text' id='title' onChange={inputChange} className='addTaskForm__text' value={title}/>
                 <label htmlFor='description' className='addTaskForm__label'>Task description</label>
                 <textarea id='description' onChange={inputChange} className='addTaskForm__textarea' value={description}/>
                 <Button type='submit' clicked={clicked} btnStyle='button--dark'>
