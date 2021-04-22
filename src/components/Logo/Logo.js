a578ccc728ddd0d73d44a2a8ef8718b52271836e
MaDud <martaduda@interia.eu>
2021-04-22 11:56:49 +0200

    Adjust headers

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import '../../styles/logo.scss';
 class Logo extends React.Component {
     render () {
         return (
-            <h2 className='logo'>WeeklyToDo</h2>
+            <h1 className='logo'>WeeklyToDo</h1>
         )
     }
 };
