d458d44eb95958fb8e7bb413d0f12b7ba1510227
MaDud <martaduda@interia.eu>
2021-04-23 19:39:55 +0200

    Implement tests for EmptyList

--- src/components/TasksList/EmptyList.js
+++ src/components/TasksList/EmptyList.js
@@ -6,11 +6,11 @@ class EmptyList extends React.Component {
 
     render() {
         return (
-            <article className='emptyList'>
-                <h1 className='emptyList__title'>There are no tasks!</h1>
-                <p className='emptyList__text'>Let's take a breath and relax</p>
-                <FontAwesomeIcon icon='couch' className='emptyList__icon'/>
-                <p className='emptyList__text'>or</p>
+            <article className='emptyList' data-test='emptyList'>
+                <h2 className='emptyList__title' data-test='emptyList__title'>There are no tasks!</h2>
+                <p className='emptyList__text' data-test='emptyList__text'>Let's take a breath and relax</p>
+                <FontAwesomeIcon icon='couch' className='emptyList__icon' data-test="emptyList__icon"/>
+                <p className='emptyList__text' data-test='emptyList__text'>or</p>
             </article>
         )
     }
---
+++ src/tests/TasksList/EmptyList.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
+import EmptyList from '../../components/TasksList/EmptyList';
+
+configure({adapter: new Adapter()})
+
+test('render EmptyList component', () => {
+    const wrapper = shallow(<EmptyList />);
+    const emptyList = wrapper.exists("[data-test='emptyList']");
+    expect(emptyList).toBe(true)
+});
+
+test('render EmptyList title', () => {
+    const wrapper = shallow(<EmptyList />);
+    const title = wrapper.find("[data-test='emptyList__title']");
+    expect(title.length).toBe(1)
+});
+
+test('render 2 EmptyList paragraphs', () => {
+    const wrapper = shallow(<EmptyList />);
+    const paragraphs = wrapper.find("[data-test='emptyList__text']");
+    expect(paragraphs.length).toBe(2)
+})
+
+test('render icon', () => {
+    const wrapper = shallow(<EmptyList />);
+    const icon = wrapper.exists("[data-test='emptyList__icon']");
+    expect(icon).toBe(true)
+})
